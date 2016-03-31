65a5ff649358d5fe97dd76e9097bee94c2f275d0
LeoCp <leonardocolodette0@gmail.com>
2016-03-30 23:44:06 -0300

    configIniciaisGrunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,11 @@
-/*code*/
+module.exports = function(grunt) {
+
+  //Configuração de cada plugin
+  grunt.initConfig({
+
+  });
+
+  //Onde fica os passos a serem execultados
+  grunt.registerTask('default', []);
+
+};
